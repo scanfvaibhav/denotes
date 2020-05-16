62be77257e58386118cfcbe2fe5ceff933ae4d82
vaibhav <singhvaibhav396@gmail.com>
2020-05-17 00:15:23 +0530

    1

--- server.js
+++ server.js
@@ -12,14 +12,4 @@ app.use(require('cors')());
 app.use(require('helmet')());
 app.use('/api/users', require('./routes/users'));
 
-// Production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.listen(PORT, () => console.log(`App running on port ${PORT}`)  );
