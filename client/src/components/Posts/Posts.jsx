cfcd32228404bbaa62b3a03a8fa1354fe419598e
vaibhav <singhvaibhav396@gmail.com>
2020-05-24 17:09:04 +0530

    Post

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,26 +1,50 @@
 import React,{Component} from 'react';
 class Posts extends Component {
-    constructor(props) {
-      super(props);
-    }
-     
-    render() {
-      return (
-          <div > 
-            <a href="#" >
-              <h4 >List group item heading</h4>
-              <p >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-            </a>
-            <a href="#" >
-              <h4 >List group item heading</h4>
-              <p >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-            </a> 
-            <a href="#" >
-              <h4 >List group item heading</h4>
-              <p >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-            </a> 
-          </div>
-      )
+  constructor(props) {
+    super(props);
+    this.state={
+        data:this.props.data
     }
 }
+
+
+render() {
+          let posts = [{"topic":"topicc","description":"description","details":"deetails"},
+          {"topic":"topicc","description":"description","details":"deetails"},
+          {"topic":"topicc","description":"description","details":"deetails"},
+          {"topic":"topicc","description":"description","details":"deetails"},
+          {"topic":"topicc","description":"description","details":"deetails"},
+          {"topic":"topicc","description":"description","details":"deetails"},
+          {"topic":"topicc","description":"description","details":"deetails"},
+          {"topic":"topicc","description":"description","details":"deetails"},
+          {"topic":"topicc","description":"description","details":"deetails"},
+          {"topic":"topicc","description":"description","details":"deetails"},
+        ];
+        return (
+            <div>
+            {posts.map(function(data, index){
+                return <Post key={index} data={data}/>
+              })}
+            
+            </div>
+            );  
+  }  
+} 
+function Post(props){
+  return (<div>
+              <Topic data={props.data.topic}/>
+              <Description data={props.data.description}/>
+              <Details data={props.data.details}/>
+              <hr/>
+          </div>);
+}
+function Topic(props){
+  return(<p>{props.data}</p>);
+}
+function Description(props){
+  return(<p>{props.data}</p>);
+}
+function Details(props){
+  return(<p>{props.data}</p>);
+} 
 export default Posts;
