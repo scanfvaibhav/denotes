666a65e59a06a1feefe484643f30da1c9782b05a
vaibhav <singhvaibhav396@gmail.com>
2020-08-01 12:12:38 +0530

    group

--- routes/posts.js
+++ routes/posts.js
@@ -19,15 +19,15 @@ router.get('/getContentById', async(req, res) => {
     try {
         let emailId=req.query.email;
         let titleId=req.query.titleId;
-        debugger
-        const menu = await Menu.find({email:emailId});
-        let arr = [titleId];
-        getNode(menu[0].menu,arr,false,titleId);
-
-        const posts = await Posts.find({email: emailId,titleId:arr}).sort({time:-1});
-        res.send({posts});
+        const menu =  await Menu.find({email:emailId});
+        
+        let node = menu[0].menu;
+        let arr=[];
+        await getTreeById(node,arr,titleId);
+        const posts =  await Posts.find({email: emailId,titleId:arr}).sort({time:-1});
+        res.send({ posts:posts })
         } catch(err) {
-        res.status(400).send({ error: err });
+            res.status(400).send({ error: err });
         }
     });
 
@@ -36,7 +36,7 @@ router.get('/getContentByNode', async(req, res) => {
         let emailId=req.query.email;
         let node=JSON.parse(req.query.node);
         let arr=[];
-        getNode([node],arr,true,"");
+        getNode([node],arr,"");
         
         const posts = await Posts.find({email: emailId,titleId:arr}).sort({time:-1});
         res.send({ posts })
@@ -45,19 +45,23 @@ router.get('/getContentByNode', async(req, res) => {
         }
     });
 
-var  getNode = async function(node,arr,flag,titleId){
-
+var  getNode =  function(node,arr,titleId){
     for(let i=0;i<node.length;i++){
         let id = node[i].key?node[i].key:node[i].id;
-        
-        if(flag){
-                arr.push(id);
-        }
-        if(id==titleId){
-            flag=true;
-        }
+         arr.push(id); 
         if(node[i].children && node[i].children.length>0){
-            getNode(node[i].children,arr,flag,titleId);
+            getNode(node[i].children,arr,titleId);
+        }
+    }
+};
+
+var  getTreeById =  function(node,arr,titleId ){
+    for(let i=0;i<node.length;i++){
+        let id = node[i].key?node[i].key:node[i].id;  
+        if(id==titleId){
+            getNode([node[i]],arr,titleId);
+        } else if(node[i].children && node[i].children.length>0){
+            getTreeById(node[i].children,arr,titleId);
         }
     }
 };
