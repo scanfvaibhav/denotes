774577b72db95bf86dde1a1515bee9bbbe92ea0e
vaibhaveka <vaibhav.singh@ekaplus.com>
2020-12-18 23:15:35 +0530

    db change

--- models/db.js
+++ models/db.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 // Connection 
 const local='mongodb://localhost:27017/Development?readPreference=primary&ssl=false';
 const prod = 'mongodb://denotes:admin123@ds229258.mlab.com:29258/heroku_q5xxkr0p';
-const atlas = "mongodb://admin:admin@denotes.iemcb.mongodb.net/denotes";
+const atlas = "mongodb+srv://admin:admin@denotes.iemcb.mongodb.net/denotes?retryWrites=true&w=majority";
 
 
 const globalUri='mongodb://germancutraro:germancutraro33@ds131551.mlab.com:31551/crud-mern';
